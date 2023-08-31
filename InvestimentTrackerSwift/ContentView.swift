d362864eae21d835dbe68ac54e8830f36222c903
Horacio Mota <horaciomota93@gmail.com>
2023-08-31 21:44:33 +0100

    Initial Commit

---
+++ InvestimentTrackerSwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  InvestimentTrackerSwift
+//
+//  Created by Horacio Mota on 31/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          Text("If you save 200 euros. In 3 years you gonna have €3000")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InvestimentTrackerSwift/InvestimentTrackerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestimentTrackerSwiftApp.swift
+//  InvestimentTrackerSwift
+//
+//  Created by Horacio Mota on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct InvestimentTrackerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
